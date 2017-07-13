829c881aab0b5bc97841d7b427f6794c3fd0ff25
reotrax <reo.trax127@gmail.com>
2017-07-14 07:06:49 +0900

    Reflected the database on the MPAndroidChart.

---
+++ app/src/main/java/android/library/com/android_library/android/library/com/android_library/fragment/parts/BarGenre.java
@@ -0,0 +1,28 @@
+package android.library.com.android_library.android.library.com.android_library.fragment.parts;
+
+/**
+ * Created by reo on 2017/07/14.
+ */
+
+public class Table {
+
+    class PieGenre {
+        String genre;
+        int money;
+
+        public PieGenre(String genre, int money) {
+            this.genre = genre;
+            this.money = money;
+        }
+    }
+
+    class BarGenre {
+        String genre;
+        int money;
+
+        public BarGenre(String genre, int money) {
+            this.genre = genre;
+            this.money = money;
+        }
+    }
+}
---
+++ app/src/main/java/android/library/com/android_library/android/library/com/android_library/fragment/parts/PieGenre.java
@@ -0,0 +1,8 @@
+package android.library.com.android_library.android.library.com.android_library.fragment.parts;
+
+/**
+ * Created by reo on 2017/07/14.
+ */
+
+public class PieGenre {
+}
